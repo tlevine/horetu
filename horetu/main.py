ad29f9a2369cbb889999e1814803a8f0d5898f50
Thomas Levine <_@thomaslevine.com>
2015-11-08 17:51:03 -0500

    subparsers

--- horetu/main.py
+++ horetu/main.py
@@ -1,18 +1,51 @@
 import argparse
 import inspect
+import sys
 
 from . import options
 
-def horetu(f):
+def horetu(f, name = None, description = None):
     '''
     :type f: Callable or dict
     :param f: The callable to produce the argument parser too,
         or a dict from str to callable to make subparsers.
     '''
     if hasattr(f, '__call__'):
-        p = argparse.ArgumentParser(f.__name__, description = options.description(f),
+        if name == None:
+            name = f.__name__
+        if description == None:
+            description = options.description(f)
+        p = argparse.ArgumentParser(name, description = description,
             formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-        return _horetu_one(p, f)
+        return _horetu_one(p, f)(p.parse_args())
+    else:
+        p = argparse.ArgumentParser(name, description = description,
+            formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+        g = _horetu_many(dest_prefix, parser, f)
+            
+        def fallback(_):
+            import sys
+            p.print_usage()
+            sys.exit(2)
+        args = p.parse_args()
+        f = None
+        while not hasattr(f, '__call__'):
+            f = g.get(args.command, fallback)
+        return f(args)
+
+def _horetu_many(dest_prefix, parser, f):
+    subparsers = parser.add_subparsers(dest = dest_prefix)
+    g = {}
+    for k, v in f.items():
+        if hasattr(v, '__call__'):
+            dest = dest_prefix + '_' + f.__name__
+            sp = subparsers.add_parser(dest, description = options.description(f))
+            g[k] = _horetu_many(dest, sp, f)
+        else:
+            dest = dest_prefix + '_' + k
+            sp = subparsers.add_parser(dest)
+            g[k] = _horetu_many(dest, sp, v)
+    return g
 
 def _horetu_one(parser, f):
     params = inspect.signature(f).parameters.values()
@@ -32,8 +65,5 @@ def _horetu_one(parser, f):
         args = [getattr(parsed_args, attr) for attr in positional_arguments]
         kwargs = {attr:getattr(parsed_args, attr) for attr in keyword_arguments}
         return f(*args, **kwargs)
-    if parser:
-        return g
-    else:
-        return g(p.parse_args())
+    return g
 
