117fb64f53088d5323e94baeb1bf8c8acb8cb7e0
Thomas Levine <_@thomaslevine.com>
2015-11-08 21:01:56 -0500

    j

--- horetu/main.py
+++ horetu/main.py
@@ -21,7 +21,7 @@ def horetu(f, name = None, description = None, _args = None):
     else:
         p = argparse.ArgumentParser(name, description = description,
             formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-        g = _horetu_many(dest_prefix, parser, f)
+        g = _horetu_many('', p, f)
             
         def fallback(_):
             import sys
@@ -35,17 +35,19 @@ def horetu(f, name = None, description = None, _args = None):
 
 def _horetu_many(dest_prefix, parser, f):
     subparsers = parser.add_subparsers(dest = dest_prefix)
-    g = {}
-    for k, v in f.items():
-        if hasattr(v, '__call__'):
-            dest = dest_prefix + '_' + f.__name__
-            sp = subparsers.add_parser(dest, description = options.description(f))
-            g[k] = _horetu_many(dest, sp, f)
-        else:
-            dest = dest_prefix + '_' + k
+    if hasattr(f, '__call__'):
+        sp = subparsers.add_parser(f.__name__, description = options.description(f))
+        return _horetu_one(sp, f)
+    else:
+        g = {}
+        for k, v in f.items():
+            if dest_prefix:
+                dest = dest_prefix + '_' + k
+            else:
+                dest = k
             sp = subparsers.add_parser(dest)
             g[k] = _horetu_many(dest, sp, v)
-    return g
+        return g
 
 def _horetu_one(parser, f):
     params = inspect.signature(f).parameters.values()
@@ -54,9 +56,9 @@ def _horetu_one(parser, f):
     for param in params:
         if param.kind == param.VAR_KEYWORD:
             raise ValueError('Variable keyword args (**kwargs) are not allowed.')
-        p.add_argument(options.name_or_flags(param), nargs = options.nargs(param),
-                       type = options.argtype(param), choices = options.argchoices(param),
-                       help = helps.get(param.name, ''), default = options.default(param))
+        parser.add_argument(options.name_or_flags(param), nargs = options.nargs(param),
+                            type = options.argtype(param), choices = options.argchoices(param),
+                            help = helps.get(param.name, ''), default = options.default(param))
 
     positional_arguments = [param.name for param in params if param.kind != param.VAR_KEYWORD]
     keyword_arguments = [param.name for param in params if param.kind == param.VAR_KEYWORD]
