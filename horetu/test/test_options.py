07207ece7830bb1fd05f3e27d03a212068034991
Thomas Levine <_@thomaslevine.com>
2015-11-19 01:55:40 -0500

    test name_or_flags

--- horetu/test/test_options.py
+++ horetu/test/test_options.py
@@ -1,3 +1,5 @@
+from inspect import Parameter
+
 from .. import options
 
 def test_docs():
@@ -5,3 +7,13 @@ def test_docs():
 
 def test_description():
     assert options.description(lambda:8) == ''
+
+def test_name_or_flags():
+    param = Parameter('input_file', Parameter.POSITIONAL_ONLY)
+    assert options.name_or_flags(param) == 'input-file'
+
+    param = Parameter('n_cores', Parameter.POSITIONAL_OR_KEYWORD)
+    assert options.name_or_flags(param) == '--n-cores'
+
+    param = Parameter('n', Parameter.POSITIONAL_OR_KEYWORD)
+    assert options.name_or_flags(param) == '-n'
