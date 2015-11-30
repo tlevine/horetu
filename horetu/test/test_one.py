619462db838069c6645abe49f0a5e6f0dd5fdbff
Thomas Levine <_@thomaslevine.com>
2015-11-29 23:22:40 -0500

    flags test

--- horetu/test/test_one.py
+++ horetu/test/test_one.py
@@ -16,3 +16,25 @@ cases = [
 def test_one(function, argv, result):
     parser = argparse.ArgumentParser()
     assert one(parser, function)(parser.parse_args(argv)) == result
+
+class FakeParser(object):
+    def __init__(self):
+        self.names_or_flags = set()
+    def add_argument(self, *args, **kwargs):
+        for a in args:
+            if isinstance(a, str):
+                self.names_or_flags.add(a)
+
+def g(verbose = False):
+    pass
+
+flag_cases = [
+    (f, {'-a', '-b', '-c'}),
+    (g, {'--verbose', '-v'}),
+]
+
+@pytest.mark.parametrize('function, names_or_flags', flag_cases)
+def test_flags(function, names_or_flags):
+    parser = FakeParser()
+    one(parser, function)
+    assert parser.names_or_flags == names_or_flags
