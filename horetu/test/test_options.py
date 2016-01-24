fe65b05c226bf41953b5fe4fe4a157aedaaf2008
Thomas Levine <_@thomaslevine.com>
2016-01-24 20:56:34 +0000

    test case for helps

--- horetu/test/test_options.py
+++ horetu/test/test_options.py
@@ -6,13 +6,30 @@ from .. import options
 def test_docs_empty():
     assert list(options.docs(lambda:8)) == []
 
-def test_docs():
+def test_docs_withtype():
     def f(abc):
         '''
         :param int abc: A word
         '''
         pass
     assert list(options.docs(f)) == [('abc', 'A word')]
+
+def test_docs_withouttype():
+    def hi(name: str, times: int = 8,
+           case: ('title', 'lower', 'upper') = 'title'):
+        '''
+        :param name: Name
+        :param times: Number of times to say hi
+        :param case: Case of the greeting
+        '''
+        for _ in range(times):
+            print('Hi ' + getattr(name, case)())
+
+    assert list(options.docs(hi)) == [
+        ('name', 'Name'),
+        ('times', 'Number of times to say hi'),
+        ('case', 'Case of the greeting'),
+    ]
 
 def test_description():
     assert options.description(lambda:8) == ''
