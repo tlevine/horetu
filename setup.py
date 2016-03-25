a654d49e3243e58bbb2888de25dd2b0218c6eb1b
Thomas Levine <_@thomaslevine.com>
2016-03-25 18:33:31 +0000

    remove dependencies from setup

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
 from distutils.core import setup
 
-backwards_compatibility = []
-
-# This works from Python 2.7 and 3.3 on
-try:
-    from inspect import signature
-except ImportError:
-    backwards_compatibility.append('ipython>=4.0.1')
-
-# This is needed to support Python 2.6 and below.
-try:
-    import argparse
-except ImportError:
-    backwards_compatibility.append('argparse>=1.4.0')
-
 setup(name='horetu',
       author='Thomas Levine',
       author_email='_@thomaslevine.com',
@@ -22,7 +8,6 @@ setup(name='horetu',
       packages=['horetu'],
       install_requires=[
           'inflection>=0.3.1',
-          'Sphinx>=1.3.1',
       ] + backwards_compatibility,
       extras_require={
           'docs': [
@@ -35,10 +20,8 @@ setup(name='horetu',
           'horetu[tests]',
       ],
       classifiers=[
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
       ],
-      version='0.0.7',
+      version='0.1.0',
       license='AGPL',
       )
