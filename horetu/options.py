9453c0f77e7b48b8a2300c7a439d0f77224f664e
Thomas Levine <_@thomaslevine.com>
2016-03-25 19:06:44 +0000

    Automatic commit with j

--- horetu/options.py
+++ horetu/options.py
@@ -56,7 +56,6 @@ def argchoices(param):
     if isinstance(param.annotation, tuple):
         return param.annotation
 
-
 def argtype(param):
     if param.annotation == param.empty or isinstance(param.annotation, tuple) \
             or (isinstance(param.default, list) and issubclass(param.annotation, list)):
@@ -65,42 +64,13 @@ def argtype(param):
         return param.annotation
 
 
-def name_or_flag(has_keyword_only, param):
-    def _name_or_flag(p):
-        name = p.name.replace('_', '-')
-        if p.default == p.empty or (
-                has_keyword_only and p.kind == p.POSITIONAL_OR_KEYWORD):
-            return p.name
-        elif len(name) == 1:
-            return '-' + name
-        else:
-            return '--' + name
-    try:
-        if issubclass(param.annotation, list):
-            return singularize(_name_or_flag(param))
-    except TypeError:
-        pass
-    return _name_or_flag(param)
+def name(param):
+    return p.name.replace('_', '-')
 
 
-def has_keyword_only(params):
-    return len(params) and params[-1].kind == params[-1].KEYWORD_ONLY
-
-
-def dest(param):
-    return param.name
-
-
-def default(param):
-    if param.default != param.empty and not isinstance(param.default, bool):
-        return param.default
-
-
-def action(param):
-    BOOL_ACTIONS = {True: 'store_false', False: 'store_true'}
-    if isinstance(param.default, bool) and param.default in BOOL_ACTIONS:
-        return BOOL_ACTIONS[param.default]
-    return {
-        Count: 'count',
-        list: 'append',
-    }.get(param.annotation, 'store')
+def flag(param):
+    x = name(param)
+    if len(x) == 1:
+        return '-' + name
+    else:
+        return '--' + name
