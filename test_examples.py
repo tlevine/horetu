f7cc5720bf4b61eff8649c4fe4d08f3ddf6d36e4
Thomas Levine <_@thomaslevine.com>
2016-03-29 20:03:06 +0000

    more example testcases

--- test_examples.py
+++ test_examples.py
@@ -7,8 +7,15 @@ testcases = [
     ('getitem', [], 2),
     ('hi', [], 2),
     ('nest', [], 2),
-#   ('requests', [], 0),
     ('spacecraft', [], 2),
+
+    ('do_something.py', ['a file', 'another file'], 0),
+    ('getitem', ['namedtuple', '-n', '2'], 0),
+    ('hi', ['Tom'], 0),
+    ('nest', ['subcommand2', '8'], 0),
+    ('spacecraft', ['mars'], 0),
+
+#   ('requests', [], 0),
 ]
 @pytest.mark.parametrize('fn, args, returncode', testcases)
 def test_example(fn, args, returncode):
